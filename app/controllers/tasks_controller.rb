df5bdce3e090928f48b9dae08e9df17687c29504
lukew244 <luke.webster86@gmail.com>
2017-03-18 14:16:48 +0000

    adds task crud actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,17 +6,40 @@ class TasksController < ApplicationController
 
 
   def new
+    @task = Task.new
   end
 
   def create
     @task = Task.new(task_params)
-    @task.save
-    redirect_to '/tasks'
+    if @task.save
+      redirect_to '/tasks'
+    else
+      render 'new'
+    end
   end
 
-  def show
+  def edit
+    @task = Task.find(params[:id])
   end
 
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      flash[:notice] = 'Task updated successfully'
+
+      redirect_to '/tasks'
+    else
+      render 'edit'
+    end
+  end
+
+def destroy
+  @task = Task.find(params[:id])
+  @task.destroy
+  flash[:notice] = 'Task deleted successfully'
+  redirect_to '/tasks'
+end
 
 private
 
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,6 @@
 class Task < ApplicationRecord
+  validates :description, presence: true, length: { maximum: 500 }
+  validates :person, presence: true
+
+
 end
